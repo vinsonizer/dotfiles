9b6eb6604fdf8b2fd9028e3d30386670d443959e
Jason Vinson <vinsonizer@gmail.com>
2017-01-02 15:35:44 -0500

    added installer

---
+++ install.sh
@@ -0,0 +1,6 @@
+ln -s compton.conf  ${HOME}/.compton.conf
+ln -s i3blocks.conf ${HOME}/.i3blocks.conf
+ln -s i3            ${HOME}/.config/i3
+ln -s i3blocks      ${HOME}/.config/i3blocks
+ln -s termite       ${HOME}/.config/termite
+ln -s powerline     ${HOME}/.config/powerline
