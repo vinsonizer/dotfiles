8886a31cab37c0f641967372bd6276fa92a8a66d
Jason Vinson <vinsonizer@gmail.com>
2017-01-17 09:27:18 -0500

    Added xinitrc and updated installer and i3 configuration

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 export localdir=`pwd`
 
+ln -s $localdir/xinitrc       ${HOME}/.xinitrc
 ln -s $localdir/compton.conf  ${HOME}/.compton.conf
 ln -s $localdir/i3blocks.conf ${HOME}/.i3blocks.conf
 ln -s $localdir/i3            ${HOME}/.config/i3
 ln -s $localdir/i3blocks      ${HOME}/.config/i3blocks
 ln -s $localdir/termite       ${HOME}/.config/termite
-ln -s $localdir/lemonbar     ${HOME}/.config/lemonbar
+ln -s $localdir/lemonbar      ${HOME}/.config/lemonbar
