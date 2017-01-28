cd6576235e7ad855b047e6229e832e87e45e6ab2
Jason Vinson <jason@localhost.localdomain>
2017-01-28 09:07:32 -0500

    added polybar

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ ln -s $localdir/i3blocks.conf ${HOME}/.i3blocks.conf
 ln -s $localdir/i3            ${HOME}/.config/i3
 ln -s $localdir/i3blocks      ${HOME}/.config/i3blocks
 ln -s $localdir/termite       ${HOME}/.config/termite
-ln -s $localdir/lemonbar      ${HOME}/.config/lemonbar
+ln -s $localdir/polybar       ${HOME}/.config/polybar
