688c82adea190016a647d14126d574107f583508
Jason Vinson <vinsonizer@gmail.com>
2017-08-18 12:51:53 -0400

    added more modes to rofi

--- scripts/rofi.sh
+++ scripts/rofi.sh
@@ -9,4 +9,6 @@ bgimage=$( cat ${HOME}/.fehbg | awk {'print $3'} | tr \' ' ')
 ## Pull 4 main colors from background file
 #colors=$(convert /tmp/bg.jpg -scale 1x3\! txt:- | awk {' print $3 '} | tail -3 | sort -r | paste -s -d ,)
 
-rofi -modi drun -show drun -width $width -lines $lines -columns $columns #-color-window "$colors"
+#rofi -modi "drun,window" -show drun -sidebar-mode -width $width -lines $lines -columns $columns #-color-window "$colors"
+
+rofi -show drun -sidebar-mode -width $width -lines $lines -columns $columns -fake-transparency
