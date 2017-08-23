f49f480402ff3fc12c2a3985b084952f3e277e44
Jason Vinson <vinsonizer@gmail.com>
2017-08-23 07:22:25 -0400

    locker uses dm-tool and added click handlers to package module

--- scripts/locker.sh
+++ scripts/locker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scrot /tmp/bg.png && mogrify -blur 4x8 /tmp/bg.png
-/usr/bin/i3lock -i /tmp/bg.png
+#scrot /tmp/bg.png && mogrify -blur 4x8 /tmp/bg.png
+#/usr/bin/i3lock -i /tmp/bg.png
 /usr/bin/dm-tool switch-to-greeter
