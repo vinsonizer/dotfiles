c238f6c3457017780543f2e42332ba1bc9c92d0a
Jason Vinson <vinsonizer@gmail.com>
2017-10-15 11:56:24 -0400

    changed intervals for script modules and removed notify send

--- polybar/pkg.sh
+++ polybar/pkg.sh
@@ -6,7 +6,7 @@ check=$((pac + aur))
 if [[ "$check" != "0" ]]
 then
     echo "$pac %{F#5b5b5b}%{F-} $aur"
-    /usr/bin/notify-send "Package Updates" "Found $pac Core and $aur AUR Updates"
+    #/usr/bin/notify-send "Package Updates" "Found $pac Core and $aur AUR Updates"
 else
     echo ""
 fi
