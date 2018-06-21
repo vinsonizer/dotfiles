f7028b0e4bcae97bdbbd01fbd5dbc34434d71c5d
Jason Vinson <vinsonizer@gmail.com>
2018-06-21 11:55:43 -0400

    added yay package counter

--- polybar/pkg.sh
+++ polybar/pkg.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-pac=$(checkupdates | wc -l)
-aur=$(cower -u | wc -l)
 
-check=$((pac + aur))
+check=$(yay -Pn)
 if [[ "$check" != "0" ]]
 then
     echo "$check updates"
-    #echo "$pac %{F#5b5b5b}%{F-} $aur"
-    #/usr/bin/notify-send "Package Updates" "Found $pac Core and $aur AUR Updates"
 else
     echo ""
 fi
