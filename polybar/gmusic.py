303dcffe9bca3714c259260ebf4816a0050069ca
Jason Vinson <vinsonizer@gmail.com>
2018-07-20 14:37:38 -0400

    added strip function

--- polybar/gmusic.py
+++ polybar/gmusic.py
@@ -7,7 +7,7 @@ home = os.environ['HOME']
 with open(home + "/.config/Google Play Music Desktop Player/json_store/playback.json") as f:
     data = json.load(f)
 
-artist= data["song"]["artist"]
-title = data["song"]["title"]
+artist= str.strip(data["song"]["artist"])
+title = str.strip(data["song"]["title"])
 
 print("%s: %s" % (artist, title))
